de5a0547b1c9bd3bbb630a79a9496979db17717b
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-10-17 18:39:28 +0300

    Fix

--- src/components/Features/FeatureAnimation.svelte
+++ src/components/Features/FeatureAnimation.svelte
@@ -1,39 +1,28 @@
 <script>
 	import { base } from '$app/paths';
 	import { onMount, onDestroy } from 'svelte';
-	import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
+	import lottie from 'lottie-web';
 
 	export let id, path, isActive;
 
-	// onMount(() => {
-	// 	lottie.loadAnimation({
-	// 		container: document.getElementById(`lottie-container-${id}`),
-	// 		renderer: 'svg',
-	// 		loop: true,
-	// 		autoplay: true,
-	// 		path: `${base}/animations/${path}.json`
-	// 	});
-	// });
+	onMount(() => {
+		lottie.loadAnimation({
+			container: document.getElementById(`lottie-container-${id}`),
+			renderer: 'svg',
+			loop: true,
+			autoplay: true,
+			path: `${base}/animations/${path}.json`
+		});
+	});
 
-	// onDestroy(() => {
-	// 	if (animation) {
-	// 		animation.destroy();
-	// 	}
-	// });
+	onDestroy(() => {
+		if (animation) {
+			animation.destroy();
+		}
+	});
 </script>
 
-<!-- <div id="lottie-container-{id}" class:active={isActive}></div> -->
-
-<LottiePlayer
-	src={`${base}/animations/${path}.json`}
-	autoplay={true}
-	loop={true}
-	controls={false}
-	renderer="svg"
-	background="transparent"
-	height={496}
-	width={471}
-/>
+<div id="lottie-container-{id}" class:active={isActive}></div>
 
 <style>
 	div {
