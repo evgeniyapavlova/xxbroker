7b7b51e7f0ed730aa9fef7cf28a034230e13cc56
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-10-17 20:26:57 +0300

    Deploy

--- _app/immutable/chunks/paths.GmZ1LeFW.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_1tma1ar)==null?void 0:s.base)??"/xxbroker";var a;const e=((a=globalThis.__sveltekit_1tma1ar)==null?void 0:a.assets)??t;export{e as a,t as b};
