5463e900a15dfd1847c8347d7c235a6e1610f597
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-10-17 18:31:57 +0300

    Deploy

---
+++ _app/immutable/chunks/paths.C1b6uPQP.js
@@ -0,0 +1,1 @@
+var s;const i=((s=globalThis.__sveltekit_v6iiim)==null?void 0:s.base)??"/xxbroker";var e;const a=((e=globalThis.__sveltekit_v6iiim)==null?void 0:e.assets)??i;export{a,i as b};
--- src/components/Features/FeatureAnimation.svelte
+++ src/components/Features/FeatureAnimation.svelte
@@ -1,28 +1,39 @@
 <script>
 	import { base } from '$app/paths';
 	import { onMount, onDestroy } from 'svelte';
-	import lottie from 'lottie-web';
+	import { LottiePlayer } from '@lottiefiles/svelte-lottie-player';
 
 	export let id, path, isActive;
 
-	onMount(() => {
-		lottie.loadAnimation({
-			container: document.getElementById(`lottie-container-${id}`),
-			renderer: 'svg',
-			loop: true,
-			autoplay: true,
-			path: `${base}/animations/${path}.json`
-		});
-	});
+	// onMount(() => {
+	// 	lottie.loadAnimation({
+	// 		container: document.getElementById(`lottie-container-${id}`),
+	// 		renderer: 'svg',
+	// 		loop: true,
+	// 		autoplay: true,
+	// 		path: `${base}/animations/${path}.json`
+	// 	});
+	// });
 
-	onDestroy(() => {
-		if (animation) {
-			animation.destroy();
-		}
-	});
+	// onDestroy(() => {
+	// 	if (animation) {
+	// 		animation.destroy();
+	// 	}
+	// });
 </script>
 
-<div id="lottie-container-{id}" class:active={isActive}></div>
+<!-- <div id="lottie-container-{id}" class:active={isActive}></div> -->
+
+<LottiePlayer
+	src={`${base}/animations/${path}.json`}
+	autoplay={true}
+	loop={true}
+	controls={false}
+	renderer="svg"
+	background="transparent"
+	height={496}
+	width={471}
+/>
 
 <style>
 	div {
