194d18a5ac8934d87508eae153239099a8ff8704
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-10-21 17:33:35 +0300

    Deploy

---
+++ _app/immutable/chunks/paths.CM47klUS.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_kew4ja)==null?void 0:s.base)??"/xxbroker";var e;const t=((e=globalThis.__sveltekit_kew4ja)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ _app/immutable/chunks/reg_link.BAeNHSEI.js
@@ -0,0 +1,1 @@
+import{w as o}from"./index.CMxgLFwr.js";const a=o(""),s="https://trade.xxbroker.com/",n=o(s),r=o(s),i=o(s);export{r as a,n as b,a as c,i as l};
