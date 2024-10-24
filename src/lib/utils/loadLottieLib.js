d945ce565fffb1c3539da8ae00c5fb9552e767c9
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2024-10-24 14:37:45 +0300

    Fixes

--- src/lib/utils/loadLottieLib.js
+++ src/lib/utils/loadLottieLib.js
@@ -2,7 +2,7 @@ import { isLottieLoaded } from '$lib/stores/isLottieLoaded';
 
 export function loadLottieLib() {
 	const script = document.createElement('script');
-	script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.12.2/lottie_svg.min.js';
+	script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.12.2/lottie_light.min.js';
 	script.defer = true;
 	// script.async = true;
 
